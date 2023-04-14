baec00f242b881873a4d49f0cef5f22160534691
Tiffany Bissonnette <tiffanybissonnette@Tiffanys-MacBook-Air.local>
2023-04-13 22:12:34 -0400

    final

--- js/main.js
+++ js/main.js
@@ -20,3 +20,53 @@ function replaceEvens (arr) {
     console.log(arr)
 };
 replaceEvens(given_arr);
+
+/* def repeat_str(repeat, string):
+    return repeat * string */
+
+/*My friend wants a new band name for her band. 
+She like bands that use the formula: "The" + a noun with 
+the first letter capitalized, for example:
+
+"dolphin" -> "The Dolphin"
+
+However, when a noun STARTS and ENDS with the same letter, 
+she likes to repeat the noun twice and connect them together 
+with the first and last letter, combined into one word 
+(WITHOUT "The" in front), like this:
+
+"alaska" -> "Alaskalaska"
+
+Complete the function that takes a noun as a string, 
+and returns her preferred band name written as a string. */
+
+let bandname = 'hand'
+let new_bandname = ''
+
+function bandname_genrator(name){
+    if (name.slice(0,1) === name.slice(-1)){
+        new_bandname= name.charAt(0).toUpperCase() + name.slice(1,-1) + name
+        new_bandname.charAt(0).toUpperCase()
+    } else {
+        new_bandname= 'The ' + name.charAt(0).toUpperCase() + name.slice(1)
+    };
+    console.log(new_bandname);
+};
+
+bandname_genrator(bandname);
+
+/* Write a function that accepts an integer n and a string s
+ as parameters, and returns a string of s repeated exactly 
+ n times. */
+
+/* def repeat_str(repeat, string):
+    return repeat * string */
+
+let random_string = "Hello"
+let new_string = ''
+
+function repeatString(num, str){
+    console.log(str.repeat(num));
+};
+
+repeatString(4, random_string);
